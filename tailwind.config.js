2748a07429b605d7fa9a41be75a74fab6cf8651f
NyctibiusVII <matheus.dev.07@gmail.com>
2023-04-24 11:33:22 -0300

    chore: Project setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,33 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      'transparent': 'transparent',
+      'white': '#ffffff',
+      'gray-50': '#f9fafb',
+      'gray-10': '#f3f4f6',
+      'gray-20': '#e5e7eb',
+      'gray-30': '#d1d5db',
+      'gray-40': '#9ca3af',
+      'gray-50': '#6b7280',
+      'gray-60': '#4b5563',
+      'gray-70': '#374151',
+      'gray-80': '#1f2937',
+      'gray-90': '#111827',
+      'gray-95': '#030712',
+      'black': '#000000',
+      'red-600': '#dc2626'
     },
-  },
-  plugins: [],
+    opacity: {
+      '0': '0',
+      '20': '0.2',
+      '40': '0.4',
+      '60': '0.6',
+      '80': '0.8',
+      '100': '1'
+    }
+  }
 }
