ad93a26772fc7a37693bd9d7cca595cc3f5bafc5
NyctibiusVII <matheus.dev.07@gmail.com>
2023-05-31 21:21:45 -0300

    feat: System theme in `sweetalert2` alerts

--- src/hooks/useSwalTheme.tsx
+++ src/hooks/useSwalTheme.tsx
@@ -1,17 +1,18 @@
 import { useTheme } from 'next-themes'
 
 export const useSwalTheme = () => {
-    const { theme } = useTheme()
+    const { systemTheme, theme } = useTheme()
+    const currentTheme = theme === 'system' ? systemTheme : theme
 
     const swalColors = {
-        bg: theme === 'dark' ? '#111827' : '#ffffff',
-        fg: theme === 'dark' ? '#f3f4f6' : '#374151',
+        bg: currentTheme === 'dark' ? '#111827' : '#ffffff',
+        fg: currentTheme === 'dark' ? '#f3f4f6' : '#374151',
         accent: '#8b5cf6',
         info: { icon: '#2778c4' },
         error: { icon: '#f87171' },
         success: { icon: '#4ade80' },
         warning: { icon: '#fcd34d' },
-        question: { icon: theme === 'dark' ? '#334155' : '#cbd5e1'},
+        question: { icon: currentTheme === 'dark' ? '#334155' : '#cbd5e1'},
         button: {
             confirm: '#2778c4',
             cancel: '#f87171'
