19b9cbbc5e4d47a888328f772f600fd520ef3f4c
NyctibiusVII <matheus.dev.07@gmail.com>
2023-07-04 23:55:00 -0300

    fix: Improper log

--- src/utils/generateUniqueRandomNumber.ts
+++ src/utils/generateUniqueRandomNumber.ts
@@ -3,10 +3,8 @@ let previousNumber = -1
 export function generateUniqueRandomNumber() {
     let randomNumber: number
 
-    do {
-        randomNumber = Math.floor(Math.random() * 100) + 1
-        console.log(previousNumber, randomNumber)
-    } while (randomNumber === previousNumber)
+    do { randomNumber = Math.floor(Math.random() * 100) + 1 }
+    while (randomNumber === previousNumber)
 
     previousNumber = randomNumber
 
